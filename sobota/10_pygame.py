f1a8f76396d01f7408c7534a05c56e2ae3355d61
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-21 13:10:33 +0100

    sobota

--- sobota/10_pygame.py
+++ sobota/10_pygame.py
@@ -11,5 +11,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                run = False
 
     sleep(.1)
