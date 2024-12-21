e048780774d34029cf232309373ac659b59205b7
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-21 10:15:35 +0100

    sobota

--- sobota/4_obiekty.py
+++ sobota/4_obiekty.py
@@ -8,9 +8,12 @@ class Samochod:
     def przyspiesz(self):
         self.predkosc += 10
         print(f"Samochód {self.marka} przyspiesza do prędkości {self.predkosc}km/h")
-
-
-
+    def hamuj(self):
+        if self.predkosc > 0:
+            self.predkosc -= 10
+            print(f"Samochód {self.marka} hamuje do prędkości {self.predkosc}km/h")
+        else:
+            print(f"Samochód {self.marka} już stoi.")
 
 samochod1 = Samochod("BMW", "e3")
 samochod2 = Samochod("Fiat", "126")
@@ -19,7 +22,13 @@ while True:
     samochod = int(input("Który samochod (1-2):"))
     co = int(input("Co ma zrobić (1-przyspiesz, 2-hamuj):"))
 
-    if samochod == 1 and co == 1:
-        samochod1.przyspiesz()
-    elif samochod == 2 and co == 1:
-        samochod2.przyspiesz()
+    if samochod == 1:
+        if co == 1:
+            samochod1.przyspiesz()
+        elif co == 2:
+            samochod1.hamuj()
+    elif samochod == 2:
+        if co == 1:
+            samochod2.przyspiesz()
+        elif co == 2:
+            samochod2.hamuj()
