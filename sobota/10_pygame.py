668d78d1ce8c09417c50603ecadb4071e1b2a143
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-21 13:42:37 +0100

    sobota

--- sobota/10_pygame.py
+++ sobota/10_pygame.py
@@ -5,7 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Aplikacja pygame")
 
-
+button = False
 run = True
 while run:
     for event in pygame.event.get():
@@ -16,13 +16,19 @@ while run:
                 run = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
+                button = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                button = False
+        elif event.type == pygame.MOUSEMOTION:
+            if button:
                 mouse_x, mouse_y = event.pos
                 #print(mouse_x,mouse_y)
                 #screen.set_at((mouse_x,mouse_y), (0,255,0))
                 #pygame.draw.rect(screen, (255,255,255), (mouse_x-10, mouse_y-10, 20, 20))
-                #pygame.draw.circle(screen, (255,0,0), (mouse_x, mouse_y), 5)
-                pygame.draw.rect(screen, 'white', (mouse_x, mouse_y, 20, 10))
-                pygame.draw.rect(screen, 'red', (mouse_x, mouse_y+10, 20, 10))
+                pygame.draw.circle(screen, (255,255,255), (mouse_x, mouse_y), 5)
+                #pygame.draw.rect(screen, 'white', (mouse_x, mouse_y, 20, 10))
+                #pygame.draw.rect(screen, 'red', (mouse_x, mouse_y+10, 20, 10))
     pygame.display.flip()
 
-    sleep(.1)
+    sleep(.01)
