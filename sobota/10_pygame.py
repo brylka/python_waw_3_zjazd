23e1cffcd593a3a35283b8acf0ced20b5cba5182
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-21 13:58:32 +0100

    sobota

--- sobota/10_pygame.py
+++ sobota/10_pygame.py
@@ -6,6 +6,7 @@ screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Aplikacja pygame")
 
 button = False
+color = (255,255,255)
 run = True
 while run:
     for event in pygame.event.get():
@@ -14,6 +15,10 @@ while run:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 run = False
+            elif event.key == pygame.K_r:
+                color = (255,0,0)
+            elif event.key == pygame.K_c:
+                screen.fill((0,0,0))
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 button = True
@@ -26,7 +31,7 @@ while run:
                 #print(mouse_x,mouse_y)
                 #screen.set_at((mouse_x,mouse_y), (0,255,0))
                 #pygame.draw.rect(screen, (255,255,255), (mouse_x-10, mouse_y-10, 20, 20))
-                pygame.draw.circle(screen, (255,255,255), (mouse_x, mouse_y), 5)
+                pygame.draw.circle(screen, color, (mouse_x, mouse_y), 5)
                 #pygame.draw.rect(screen, 'white', (mouse_x, mouse_y, 20, 10))
                 #pygame.draw.rect(screen, 'red', (mouse_x, mouse_y+10, 20, 10))
     pygame.display.flip()
