7b368f551c38f12e5becd740b491b4a58c483361
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-21 13:35:46 +0100

    sobota

--- sobota/10_pygame.py
+++ sobota/10_pygame.py
@@ -18,7 +18,11 @@ while run:
             if event.button == 1:
                 mouse_x, mouse_y = event.pos
                 #print(mouse_x,mouse_y)
-                screen.set_at((mouse_x,mouse_y), (0,255,0))
+                #screen.set_at((mouse_x,mouse_y), (0,255,0))
+                #pygame.draw.rect(screen, (255,255,255), (mouse_x-10, mouse_y-10, 20, 20))
+                #pygame.draw.circle(screen, (255,0,0), (mouse_x, mouse_y), 5)
+                pygame.draw.rect(screen, 'white', (mouse_x, mouse_y, 20, 10))
+                pygame.draw.rect(screen, 'red', (mouse_x, mouse_y+10, 20, 10))
     pygame.display.flip()
 
     sleep(.1)
