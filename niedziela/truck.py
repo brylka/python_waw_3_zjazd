12227110a23348c5c8c9a56dd21b3206f0cd0d21
Bartosz Bryniarski <brylka@jelenia.com>
2024-12-22 09:36:32 +0100

    niedziela

--- niedziela/truck.py
+++ niedziela/truck.py
@@ -4,6 +4,8 @@ class Truck(Vehicle):
     def __init__(self, mark, model, year, capacity):
         super().__init__(mark, model, year)
         self.capacity = capacity
+    def __str__(self):
+        return f"Klasa nadrzędna: {super().__str__()} - Obiekt z klasy Truck"
 
     def load(self):
         print(f"Samochód {self.mark} został załadowany!")
@@ -12,3 +14,4 @@ if __name__ == '__main__':
     truck = Truck("Star", "25", 1970, 10)
     truck.hong()
     truck.load()
+    print(truck)
--- niedziela/vehicle.py
+++ niedziela/vehicle.py
@@ -4,10 +4,13 @@ class Vehicle:
         self.model = model
         self.year = int(year)
         self.color = color
+    def __str__(self):
+        return f"Jestem obiekt z klasy Vehicle - marka {self.mark}"
     def hong(self):
         print(f"Samochód {self.color} {self.mark} trąbi!")
 
 if __name__ == '__main__':
     vehicle = Vehicle("BMW", "e30", "2000", "niebieski")
     vehicle.hong()
+    print(vehicle)
     #print("ala ma kota")
